fc0ec64149d1ff57de6dba561a3ad988e3783d80
masimacbook <masi19bw@gmail.com>
2021-08-21 11:17:31 +0900

    Add index to isu

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -12,6 +12,8 @@ CREATE TABLE `isu` (
   `jia_user_id` VARCHAR(255) NOT NULL,
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
   `updated_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  INDEX idx_jia_user_id (jia_user_id, id),
+  INDEX idx_chara (character),
    PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
