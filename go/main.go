b1a0dbdb4246c62f04bedf537619a97cb5f95595
sanposhiho <44139130+sanposhiho@users.noreply.github.com>
2021-08-21 13:30:24 +0900

    f

--- go/main.go
+++ go/main.go
@@ -1185,7 +1185,7 @@ var mu sync.Mutex
 func bulkloop() {
 	isuconlist = nil
 	go func() {
-		for range time.Tick(900 * time.Millisecond) {
+		for range time.Tick(300 * time.Millisecond) {
 			if len(isuconlist) == 0 {
 				fmt.Println("pass")
 				continue
